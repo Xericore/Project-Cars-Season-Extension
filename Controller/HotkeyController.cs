6494ac6f2520d08cf8777b8a286e9db16578ed25
Mario Petricevic <Mario.Petricevic@3ds.com>
2016-12-05 12:06:30 +0100

    Refactors code.

--- Controller/HotkeyController.cs
+++ Controller/HotkeyController.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
 
 namespace ProjectCarsSeasonExtension.Controller
 {
-    class HotkeyController
+    internal class HotkeyController
     {
+        public delegate void KeyHook();
+
+        public const uint None = 0x0000;
+        public const uint ModAlt = 0x0001;
+        public const uint ModControl = 0x0002;
+        public const uint ModNorepeat = 0x4000;
+        public const uint ModShift = 0x0004;
+        public const uint ModWin = 0x0008;
+
+        private static HotkeyController _instance;
+        private readonly WindowInteropHelper _helper;
+        private readonly Dictionary<int, KeyHook> _keyHookMap;
+        private HwndSource _source;
+
+        private HotkeyController(Window window)
+        {
+            _keyHookMap = new Dictionary<int, KeyHook>();
+            _helper = new WindowInteropHelper(window);
+            _source = HwndSource.FromHwnd(_helper.Handle);
+            _source?.AddHook(HwndHook);
+        }
+
         [DllImport("User32.dll")]
         private static extern bool RegisterHotKey(
             [In] IntPtr hWnd,
@@ -23,28 +42,6 @@ namespace ProjectCarsSeasonExtension.Controller
             [In] IntPtr hWnd,
             [In] int id);
 
-        public const uint NONE = 0x0000;
-        public const uint MOD_ALT = 0x0001;
-        public const uint MOD_CONTROL = 0x0002;
-        public const uint MOD_NOREPEAT = 0x4000;
-        public const uint MOD_SHIFT = 0x0004;
-        public const uint MOD_WIN = 0x0008;
-
-        public delegate void KeyHook();
-
-        private static HotkeyController instance;
-        private WindowInteropHelper helper;
-        private HwndSource _source;
-        private Dictionary<int, KeyHook> keyHookMap;
-
-        private HotkeyController(Window window)
-        {
-            keyHookMap = new Dictionary<int, KeyHook>();
-            helper = new WindowInteropHelper(window);
-            _source = HwndSource.FromHwnd(helper.Handle);
-            _source.AddHook(HwndHook);
-        }
-
         ~HotkeyController()
         {
             Clear();
@@ -52,36 +49,35 @@ namespace ProjectCarsSeasonExtension.Controller
 
         public static void Init(Window window)
         {
-            if (instance == null)
+            if (_instance == null)
             {
-                instance = new HotkeyController(window);
+                _instance = new HotkeyController(window);
             }
         }
 
-        private static HotkeyController getInstance()
+        private static HotkeyController GetInstance()
         {
-            if (instance == null)
+            if (_instance == null)
             {
                 throw new Exception("HotkeyController not initialized");
             }
-            return instance;
+            return _instance;
         }
 
         public static void Register(int hotkeyId, uint fsModifiers, uint vk, KeyHook hook)
         {
-            getInstance().InternalRegister(hotkeyId, fsModifiers, vk, hook);
+            GetInstance().InternalRegister(hotkeyId, fsModifiers, vk, hook);
         }
 
         private void InternalRegister(int hotkeyId, uint fsModifiers, uint vk, KeyHook hook)
         {
-
-            if(keyHookMap.ContainsKey(hotkeyId))
+            if (_keyHookMap.ContainsKey(hotkeyId))
             {
                 throw new Exception("hotkeyId already in use");
             }
 
-            keyHookMap.Add(hotkeyId, hook);
-            if (!RegisterHotKey(helper.Handle, hotkeyId, fsModifiers, vk))
+            _keyHookMap.Add(hotkeyId, hook);
+            if (!RegisterHotKey(_helper.Handle, hotkeyId, fsModifiers, vk))
             {
                 throw new Exception("Hotkey could not be registered");
             }
@@ -89,22 +85,22 @@ namespace ProjectCarsSeasonExtension.Controller
 
         public static void Unregister(int hotkeyId)
         {
-            getInstance().InternalUnregister(hotkeyId);
+            GetInstance().InternalUnregister(hotkeyId);
         }
 
         private void InternalUnregister(int hotkeyId)
         {
             KeyHook hook;
-            if(keyHookMap.TryGetValue(hotkeyId, out hook))
+            if (_keyHookMap.TryGetValue(hotkeyId, out hook))
             {
-                keyHookMap.Remove(hotkeyId);
-                UnregisterHotKey(helper.Handle, hotkeyId);
+                _keyHookMap.Remove(hotkeyId);
+                UnregisterHotKey(_helper.Handle, hotkeyId);
             }
         }
 
         public static void Clear()
         {
-            getInstance().InternalClear();
+            GetInstance().InternalClear();
         }
 
         private void InternalClear()
@@ -112,17 +108,17 @@ namespace ProjectCarsSeasonExtension.Controller
             _source.RemoveHook(HwndHook);
             _source = null;
 
-            foreach (KeyValuePair<int, KeyHook> entry in keyHookMap)
+            foreach (var entry in _keyHookMap)
             {
-                UnregisterHotKey(helper.Handle, entry.Key);
+                UnregisterHotKey(_helper.Handle, entry.Key);
             }
-            keyHookMap.Clear();
+            _keyHookMap.Clear();
         }
 
         private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
             KeyHook hook;
-            if(keyHookMap.TryGetValue(wParam.ToInt32(), out hook))
+            if (_keyHookMap.TryGetValue(wParam.ToInt32(), out hook))
             {
                 hook.Invoke();
             }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,12 @@
-﻿using ProjectCarsSeasonExtension.Controller;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ProjectCarsSeasonExtension.Controller;
+using ProjectCarsSeasonExtension.Views;
 
 namespace ProjectCarsSeasonExtension
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -30,15 +17,14 @@ namespace ProjectCarsSeasonExtension
 
         private void Window_Initialized(object sender, EventArgs e)
         {
-            this.Content = new Views.HighscoreView();
+            Content = new HighscoreView();
         }
 
         private void Window_SourceInitialized(object sender, EventArgs e)
         {
             HotkeyController.Init(this);
-            HotkeyController.Register(9000, HotkeyController.NONE, VirtualKeyCode.VK_ADD, () => {
-                MessageBox.Show("hotkey got clicked");
-            });
+            HotkeyController.Register(9000, HotkeyController.None, VirtualKeyCode.VkAdd,
+                () => { MessageBox.Show("hotkey got clicked"); });
         }
     }
 }
