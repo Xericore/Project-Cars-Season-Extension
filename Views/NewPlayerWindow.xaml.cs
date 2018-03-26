8231574ed7a6a4d9886a9e12705f9c5e18555aea
Mario Petricevic <mario@petricevic.net>
2018-03-26 15:09:29 +0200

    Adds validation to new player window.

--- Views/NewPlayerWindow.xaml.cs
+++ Views/NewPlayerWindow.xaml.cs
@@ -1,13 +1,34 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using ProjectCarsSeasonExtension.Annotations;
 
 namespace ProjectCarsSeasonExtension.Views
 {
     /// <summary>
     /// Interaction logic for NewPlayerWindow.xaml
     /// </summary>
-    public partial class NewPlayerWindow : Window
+    public partial class NewPlayerWindow : Window, INotifyPropertyChanged
     {
-        public string PlayerName { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public NewPlayer NewPlayer { get; set; } = new NewPlayer();
+
+        public string PlayerName => NewPlayer.Name;
+
+        private bool _isValidationPassed;
+
+        public bool IsValidationPassed
+        {
+            get => _isValidationPassed;
+            set
+            {
+                _isValidationPassed = value;
+                OnPropertyChanged();
+            }
+        }
 
         public NewPlayerWindow()
         {
@@ -17,9 +38,44 @@ namespace ProjectCarsSeasonExtension.Views
 
         private void OK_OnClick(object sender, RoutedEventArgs e)
         {
-            PlayerName = TextBoxNewPlayerName.Text;
+            NewPlayer.Name = TextBoxNewPlayerName.Text;
             DialogResult = true;
         }
 
+        private void Validation_Error(object sender, ValidationErrorEventArgs e)
+        {
+            IsValidationPassed = e.Action != ValidationErrorEventAction.Added;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class NewPlayer: IDataErrorInfo
+    {
+        public string Name { get; set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+
+                if (columnName != "Name") return null;
+
+                if (string.IsNullOrEmpty(Name))
+                    result = "Please enter a name.";
+
+                return result;
+            }
+        }
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
