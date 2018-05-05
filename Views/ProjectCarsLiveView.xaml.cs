6b6e44f57f109479230319628629040be4d828ef
Mario Petricevic <mario@petricevic.net>
2018-05-05 11:45:06 +0200

    Adds first Project Cars Shared Memory integration in ProjectCarsLiveView.

--- Views/ProjectCarsLiveView.xaml.cs
+++ Views/ProjectCarsLiveView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using pCarsAPI_Demo;
 
 namespace ProjectCarsSeasonExtension.Views
 {
@@ -20,9 +23,36 @@ namespace ProjectCarsSeasonExtension.Views
     /// </summary>
     public partial class ProjectCarsLiveView : Page
     {
+        public static int StringLengthMax = 64;
+
+        private readonly DispatcherTimer _dispatchTimer;
+        public pCarsDataClass ProjectCarsData = new pCarsDataClass();
+
         public ProjectCarsLiveView()
         {
             InitializeComponent();
+
+            DataContext = ProjectCarsData;
+
+            _dispatchTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(1) };
+            _dispatchTimer.Tick += ProjectCarsCarsDataGetterLoop;
+            _dispatchTimer.Start();
+        }
+
+        private void ProjectCarsCarsDataGetterLoop(object sender, EventArgs e)
+        {
+            var returnTuple = pCarsAPI_GetData.ReadSharedMemoryData();
+
+            //item1 is the true/false indicating a good read or not
+            if (returnTuple.Item1)
+            {
+                ProjectCarsData = ProjectCarsData.MapStructToClass(returnTuple.Item2, ProjectCarsData);
+            }
+        }
+
+        private void ProjectCarsLiveView_OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _dispatchTimer.Stop();
         }
     }
 }
