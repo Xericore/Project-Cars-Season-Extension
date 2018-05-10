aa647ff6a8cf6113623d490a9b7161afc7fd0e01
Mario Petricevic <mario@petricevic.net>
2018-05-10 14:06:24 +0200

    Updates SeasonEditor.

--- Views/SeasonEditor.xaml.cs
+++ Views/SeasonEditor.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using ProjectCarsSeasonExtension.Annotations;
 using ProjectCarsSeasonExtension.Models;
 
 namespace ProjectCarsSeasonExtension.Views
@@ -7,16 +12,41 @@ namespace ProjectCarsSeasonExtension.Views
     /// <summary>
     /// Interaction logic for SeasonEditor.xaml
     /// </summary>
-    public partial class SeasonEditor : Page
+    public partial class SeasonEditor : Page, INotifyPropertyChanged
     {
-        private readonly DataView _dataView;
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public ObservableCollection<Challenge> Challenges => _dataView.CurrentSeason.Challenges;
+        public Challenge SelectedChallenge { get; set; }
+
+        private readonly DataView _dataView;
+        
         public SeasonEditor(DataView dataView)
         {
             _dataView = dataView;
+            SelectedChallenge = Challenges.First();
+
             InitializeComponent();
         }
 
-        public ObservableCollection<Challenge> Challenges => _dataView.CurrentSeason.Challenges;
+        private void ChallengeSelector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                SelectedChallenge = e.AddedItems[0] as Challenge;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            
+            OnPropertyChanged(nameof(SelectedChallenge));
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
