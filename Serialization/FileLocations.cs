1a86a938628c0dad264012f93600f46946a56030
Mario Petricevic <mario@petricevic.net>
2018-06-25 11:25:27 +0200

    Adds tracks pngs.

--- Serialization/FileLocations.cs
+++ Serialization/FileLocations.cs
@@ -4,7 +4,7 @@ namespace ProjectCarsSeasonExtension.Serialization
 {
     public static class FileLocations
     {
-        public static string BaseFolder = AppDomain.CurrentDomain.BaseDirectory+ "saveData/";
+        public static string BaseFolder = AppDomain.CurrentDomain.BaseDirectory+ "savedata/";
         public static string PlayerFileUri = BaseFolder + "/players.xml";
         public static string PlayerResultFileUri = BaseFolder + "/playerResults.xml";
         public static string ChallengeFileUri = BaseFolder + "/challenges.xml";
