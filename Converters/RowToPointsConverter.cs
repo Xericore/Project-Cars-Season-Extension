e801618c7049d37555e878bd7bb57a593ed9b890
Mario Petricevic <mario@petricevic.net>
2018-07-15 00:01:50 +0200

    Adds points to Race view.

---
+++ Converters/RowToPointsConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using ProjectCarsSeasonExtension.Utils;
+
+namespace ProjectCarsSeasonExtension.Converters
+{
+    public class RowToPointsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var rowIndex = 0;
+            if (value is DataGridRow row)
+                rowIndex = row.GetIndex();
+
+            return PointsUtil.PositionToPoints(rowIndex + 1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
