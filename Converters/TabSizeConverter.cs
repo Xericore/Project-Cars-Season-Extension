ccbbf435a5e23bf12620d7f570590b696f873795
Mario Petricevic <Mario.Petricevic@3ds.com>
2018-07-30 16:45:20 +0200

    Fixes Races tabs flickering when only two tabs are visible.

--- Converters/TabSizeConverter.cs
+++ Converters/TabSizeConverter.cs
@@ -12,7 +12,7 @@ namespace ProjectCarsSeasonExtension.Converters
             TabControl tabControl = values[0] as TabControl;
             double width = tabControl.ActualWidth / tabControl.Items.Count;
             //Subtract 1, otherwise we could overflow to two rows.
-            return (width <= 1) ? 0 : (width - 1);
+            return (width <= 1) ? 0 : (width - 2);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
