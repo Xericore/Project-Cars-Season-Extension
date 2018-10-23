54222cf88486250e3fd0804708bfb8a2c5ddaff5
Mario Petricevic <Mario.Petricevic@3ds.com>
2018-10-23 11:17:19 +0200

    Updates Tests.

--- ProjectCarsSeasonExtensionTests/PlayersTest.cs
+++ ProjectCarsSeasonExtensionTests/PlayersTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProjectCarsSeasonExtension.Models;
+using ProjectCarsSeasonExtension.Models.Player;
 using ProjectCarsSeasonExtension.Serialization;
 using ProjectCarsSeasonExtension.ViewModels;
 using ProjectCarsSeasonExtension.Views;
@@ -10,20 +11,43 @@ namespace ProjectCarsSeasonExtensionTests
     [TestClass]
     public class PlayersTest
     {
-        private DataView _dataView;
+        private static DataView _dataView;
+        private static AllChallengeStandings _allChallengeStandings;
+        private static PlayerController _playerController;
+        private static PlayerSelection _playerSelection;
 
         [TestMethod]
         [ClassInitialize]
-        public void ReadSeasonData()
+        public static void ReadSeasonData(TestContext testContext)
         {
-            ISeasonReader seasonReader = new SeasonReaderTest();
+            ISeasonReader seasonReader = new DummySeasonReader();
             _dataView = new DataView(seasonReader);
 
-            var allChallengeStandings = new AllChallengeStandings(_dataView);
+            _allChallengeStandings = new AllChallengeStandings(_dataView);
+            _playerController = new PlayerController(_dataView);
+            _playerSelection = new PlayerSelection(_playerController);
+        }
 
-            var playerSelection = new PlayerSelection(_dataView.Players);
+        [TestMethod]
+        public void Given_SlowestChallengeResult_IsPlayerAtSamePosition()
+        {
+            var challengeResult = new ChallengeResult
+            {
+                CarName = "Formula C",
+                LastValidLapTime = new TimeSpan(0,0,0,45,000),
+                TrackLocationAndVariant = "Circuit de Barcelona-Catalunya Club"
+            };
+            var player = new Player
+            {
+                AvatarFileName = "",
+                Group = AuthenticationGroup.User,
+                Id = 0,
+                Name = "Mario"
+            };
 
-            Assert.Fail("Unfinished test.");
+            int playerPosition = _allChallengeStandings.GetPlayerPosition(challengeResult, player);
+
+            Assert.AreEqual(0, playerPosition);
         }
     }
 }
