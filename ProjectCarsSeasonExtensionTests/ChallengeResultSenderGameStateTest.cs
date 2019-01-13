9d1e81774d0e9aa058d282587ad7b151b3d13964
Mario Petricevic <mario@petricevic.net>
2019-01-13 21:40:08 +0100

    Adds unit tests.

---
+++ ProjectCarsSeasonExtensionTests/ChallengeResultSenderGameStateTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using NUnit.Framework;
+using pCarsAPI_Demo;
+using ProjectCarsSeasonExtension.ChallengeResultSender;
+
+namespace ProjectCarsSeasonExtensionTests
+{
+    [TestFixture]
+    public class ChallengeResultSenderGameStateTest
+    {
+        private ChallengeResultSender _challengeResultSender;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _challengeResultSender = new ChallengeResultSender();
+        }
+        
+        [Test]
+        public void Given_GameExited_IsEventNotFired()
+        {
+            ProjectCarsStateData projectCarsStateData = new ProjectCarsStateData(
+                carName: "Formula A", trackLocation: "Barcelona", trackVariant: "Club",
+                lastLapTime: 60, lapInvalidated: false,
+                gameState: GameState.GameExited,
+                sessionState: SessionState.SessionTimeAttack,
+                raceState: RaceState.RacestateRacing
+                );
+
+            bool eventWasFired = false;
+
+            _challengeResultSender.ChallengeResultEvent += result => { eventWasFired = true; };
+
+            _challengeResultSender.CheckProjectCarsStateData(projectCarsStateData);
+
+            Assert.That(!eventWasFired);
+        }
+        
+        [Test]
+        public void Given_GameMax_IsEventNotFired()
+        {
+            ProjectCarsStateData projectCarsStateData = new ProjectCarsStateData(
+                carName: "Formula A", trackLocation: "Barcelona", trackVariant: "Club",
+                lastLapTime: 60, lapInvalidated: false,
+                gameState: GameState.GameMax,
+                sessionState: SessionState.SessionTimeAttack,
+                raceState: RaceState.RacestateRacing
+                );
+
+            bool eventWasFired = false;
+
+            _challengeResultSender.ChallengeResultEvent += result => { eventWasFired = true; };
+
+            _challengeResultSender.CheckProjectCarsStateData(projectCarsStateData);
+
+            Assert.That(!eventWasFired);
+        }
+        
+        [Test]
+        public void Given_GameFrontEnd_IsEventNotFired()
+        {
+            ProjectCarsStateData projectCarsStateData = new ProjectCarsStateData(
+                carName: "Formula A", trackLocation: "Barcelona", trackVariant: "Club",
+                lastLapTime: 60, lapInvalidated: false,
+                gameState: GameState.GameFrontEnd,
+                sessionState: SessionState.SessionTimeAttack,
+                raceState: RaceState.RacestateRacing
+                );
+
+            bool eventWasFired = false;
+
+            _challengeResultSender.ChallengeResultEvent += result => { eventWasFired = true; };
+
+            _challengeResultSender.CheckProjectCarsStateData(projectCarsStateData);
+
+            Assert.That(!eventWasFired);
+        }
+        
+        [Test]
+        public void Given_GameIngamePaused_IsEventNotFired()
+        {
+            ProjectCarsStateData projectCarsStateData = new ProjectCarsStateData(
+                carName: "Formula A", trackLocation: "Barcelona", trackVariant: "Club",
+                lastLapTime: 60, lapInvalidated: false,
+                gameState: GameState.GameIngamePaused,
+                sessionState: SessionState.SessionTimeAttack,
+                raceState: RaceState.RacestateRacing
+                );
+
+            bool eventWasFired = false;
+
+            _challengeResultSender.ChallengeResultEvent += result => { eventWasFired = true; };
+
+            _challengeResultSender.CheckProjectCarsStateData(projectCarsStateData);
+
+            Assert.That(!eventWasFired);
+        }
+        
+        [Test]
+        public void Given_GameIngamePaused_IsEventFired()
+        {
+            ProjectCarsStateData projectCarsStateData = new ProjectCarsStateData(
+                carName: "Formula A", trackLocation: "Barcelona", trackVariant: "Club",
+                lastLapTime: 60, lapInvalidated: false,
+                gameState: GameState.GameIngamePlaying,
+                sessionState: SessionState.SessionTimeAttack,
+                raceState: RaceState.RacestateRacing
+                );
+
+            bool eventWasFired = false;
+
+            _challengeResultSender.ChallengeResultEvent += result => { eventWasFired = true; };
+
+            _challengeResultSender.CheckProjectCarsStateData(projectCarsStateData);
+
+            Assert.That(eventWasFired);
+        }
+    }
+}
